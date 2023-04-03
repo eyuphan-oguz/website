d895aeda678e92ba848c1c69e1ef9ae5f4317bc7
eyuphan-oguz <eyuphanoguz@yahoo.com>
2023-04-03 22:56:38 +0300

    create about me

--- lib/view/home_page_view.dart
+++ lib/view/home_page_view.dart
@@ -30,7 +30,7 @@ class _HomePageViewState extends State<HomePageView> {
                     flex: 5,
                     child: Container(
                       child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Column(
@@ -38,12 +38,17 @@ class _HomePageViewState extends State<HomePageView> {
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text("Hello, I am",style: TextStyle(color: Color(0xFFFCC028),fontFamily: 'Poppins',fontSize: 17,fontWeight: FontWeight.w700),),
+                              SizedBox(height: size.height*0.015,),
                               Text("CALEB NYONG",style: TextStyle(color: (Colors.black),fontFamily: 'Poppins',fontSize: 50,fontWeight: FontWeight.w700),),
+                              SizedBox(height: size.height*0.015,),
                               Text("Mobile Developer & Computer Engineer",style: TextStyle(color: Color(0xFF207733),fontFamily: 'Poppins',fontSize: 20,fontWeight: FontWeight.w500)),
+                              SizedBox(height: size.height*0.015,),
                               Text("I’m a top online marketer and branding expert, I started my caree by lauching the popular metaverse design, in just a few short years, I built the brand to millions of social media followers and websites visitors.",style: TextStyle(color: Color(0xff808080),fontFamily: 'Poppins',fontSize: 17.52,fontWeight: FontWeight.w500,overflow: TextOverflow.ellipsis,),maxLines: 4,),
+                              SizedBox(height: size.height*0.03,),
                               Row(
                                 children: [
                                   HireMeButtonWidget(size: size, ),
+                                  SizedBox(width: size.width*0.015,),
                                   TextButton(onPressed: (){}, child: Row(children: [Text("Download CV",
                                     style: TextStyle(
                                         color: Colors.black, // Beyaz font rengi
@@ -51,17 +56,35 @@ class _HomePageViewState extends State<HomePageView> {
                                         fontSize: 15,
                                         fontFamily: "Poppins"
                                     ),
-                                  ),Icon(Icons.download_rounded,color: Color(0xFFFCC028),)])),
+                                  ),
+                                    SizedBox(width: size.width*0.005,),
+                                    Icon(Icons.download_rounded,color: Color(0xFFFCC028),)])),
                                 ],
                               )
                             ],
                           ),
-                          Container(width: size.width*0.3,height: size.height*0.2,color: Colors.red,
+                          Container(width: size.width*0.3,height: size.height*0.17,color: Colors.red,
                           child: Card(
-                            child: ListTile(
-                              leading: CircleAvatar(),
-                              title: Text("Feyzanur Yesildal"),
-                              subtitle: Text("Sat 28 May 2022"),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  ListTile(
+                                    contentPadding: EdgeInsets.only(left: 0),
+                                    leading: CircleAvatar(),
+                                    title: Text("Feyzanur Yesildal"),
+                                    subtitle: Text("Sat 28 May 2022"),
+
+                                  ),
+                                  Text("In just a few short years, I built the brand to millions of social media followers and websites visitors.",
+                                  style: TextStyle(
+                                    fontFamily: 'Poppins',
+                                    fontWeight: FontWeight.w500
+                                  ),)
+                                ],
+                              ),
                             ),
                           ),)
                         ],
@@ -71,7 +94,8 @@ class _HomePageViewState extends State<HomePageView> {
                   Expanded(
                     flex: 5,
                     child: Container(
-                      color: Colors.black,
+
+                      child: Image.asset("assets/images/deneme.png",fit: BoxFit.contain,),
                     ),
                   )
                 ],
